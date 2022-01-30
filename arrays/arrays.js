5a8407b8099780e36ed642455207327a18f26d80
Paulo Victor <paulovictorlima418@gmail.com>
2022-01-30 00:02:54 -0300

    Basic functions arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,17 @@
+//ARRAYS BIDIMENSIONAIS...
+let array = [['A', 1], ['B', 2], ['C', 3], ['D', 4]];
+
+console.log(array[2][1]);
+
+// FUNÇÕES BÁSICAS DE ARRAYS...
+let array1 = ['aaa', 'bbb', 'ccc'];
+
+console.log(array1);
+
+//array.push('ddd'); //adciona elemento no final do array
+//array.unshift('xxx'); //adciona um elemento no inicio do array
+//array.pop(); //apaga o último elemento do array
+//array.shift(); //apaga o primeiro elemento do array
+let arrayPos = array1.indexOf('bbb'); //mostra a posição no array ou -1 se n encontrars
+
+console.log(arrayPos);
