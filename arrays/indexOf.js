e79cc408d77a8a555c80ea5b59200e8331780c4a
Paulo Victor <paulovictorlima418@gmail.com>
2022-01-31 21:17:22 -0300

    Array indexOf()

---
+++ arrays/indexOf.js
@@ -0,0 +1,7 @@
+//O indexOf() procura um valor de elemento em uma matriz e retorna sua POSIÇÂO ou -1
+
+const numbers = [45, 4, 9, 16, 25];
+
+let pos = numbers.indexOf(9) + 1
+
+console.log(pos);//irá retornar a posição 
