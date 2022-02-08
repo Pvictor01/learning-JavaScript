4b65353d64ef26a57b6a164f8c7ee749f4c3c695
Paulo Victor <paulovictorlima418@gmail.com>
2022-02-08 12:06:49 -0300

    Class and POO introduction

---
+++ poo/class-and-poo.js
@@ -0,0 +1,8 @@
+//classes e POO
+class spaceship {
+  //possui método construtor vazio
+}; //criando um objeto a partir de uma classe nativa do JS
+
+let mySpaceship = new spaceship();
+
+console.log(mySpaceship);
